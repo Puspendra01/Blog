793ea2d42e68d1ce0bcf5aa983d047070b0d214c
Puspendra01 <puspendrajadon142@gmail.com>
2024-02-22 22:15:18 +0530

    send

--- db/connectdb.js
+++ db/connectdb.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
  const local_url = 'mongodb://127.0.0.1:27017/pnblog'
+ const url = 'mongodb+srv://puspendrajadon142:88888888@blog.phb7ugo.mongodb.net/Blog?retryWrites=true&w=majority&appName=Blog'
 const connectDB =()=>{
     //return mongoose.connect('mongodb://localhost:27017/blog_website')
-    return mongoose.connect(local_url)
+    return mongoose.connect(url)
     .then(()=>{
         console.log('Connection successful')
     })
